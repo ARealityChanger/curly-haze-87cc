11baebee3d007afde7293526cd5a6748d30bf836
ARealityChanger <109878695+ARealityChanger@users.noreply.github.com>
2025-04-23 23:11:46 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "baby zombie riding a chicken. aka chicken jockey in minecraft",
+      prompt: "minecraft",
     };
 
     const response = await env.AI.run(
